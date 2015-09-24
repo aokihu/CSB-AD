50d91e9bc338c900f714aee1797262c43add994c
aokihu <aokihu@gmail.com>
2015-09-24 17:06:42 +0800

    有点雏形了

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 $(function(){
 
-  var backgroundColors = ['03a9f4','ff4081','03a9f4'];
+  var backgroundColors = ['1e88e5','ff4081','d81b60','ff4081'];
 
   let $body = $('body').first();
-
-  $("#welcome").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
-    $("#welcome").remove()
-  })
 
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
     direction: 'vertical',
     roundLengths:true,
-    // parallax: true,
-        speed: 300
+    parallax: true,
+    // hashnav: true,
+        speed: 500
   });
 
   $body.css({'background-color':'#'+backgroundColors[0]});
 
   // after scroll end
-  mySwiper.on('transitionEnd', (swiper) => {
+  mySwiper.on('slideChangeEnd', (swiper) => {
 
     // change background color
     let color = backgroundColors[swiper.activeIndex];
-    console.log(color)
+
     setTimeout(()=>{
       $body.css({'background':'#'+color});
     },100);
 
+    // 添加动画
+    let slide =  $('#swiper-wrapper .swiper-slide')[swiper.activeIndex];
+    $(slide).find('> *').each((index,item) => {
+      let $item = $(item);
+      let className = $item.attr('class');
+      $item.attr('class', className.replace('animate','animated'));
+    });
 
   });
 
