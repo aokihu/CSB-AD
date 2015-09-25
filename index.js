a12ded6eb7f70417fa73b5a623f6d14f52525a2a
aokihu <aokihu@gmail.com>
2015-09-25 21:03:53 +0800

    开始制作产品介绍

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ $(function(){
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
     direction: 'vertical',
-    // roundLengths:true,
-    // parallax: true,
+    roundLengths:true,
         speed: 500,
        onInit: swiper => {
           // 初始化动画效果
           // 页面加载时候隐藏所有动画元素
           swiperAnimateCache(swiper); //隐藏动画元素
           swiperAnimate(swiper); //初始化完成开始动画
+
+          let bgColor = $(swiper.slides[swiper.activeIndex]).attr('data-background-color');
+          $body.css({"background-color":bgColor})
         }
   });
-
-  $body.css({'background-color':'#'+backgroundColors[0]});
 
   // after scroll end
   mySwiper.on('slideChangeEnd', (swiper) => {
     swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
 
+    // Change background-color
+    let bgColor = $(swiper.slides[swiper.activeIndex]).attr('data-background-color');
+    $body.css({"background-color":bgColor})
   });
 
 })
