2b4dc0940827b651e4adc9e6cda54d169b8a4bae
aokihu <aokihu@gmail.com>
2015-09-27 21:43:06 +0800

    开始制作产品特性说明

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ $(function(){
     // Optional parameters
     direction: 'vertical',
     roundLengths:true,
+    lazyLoading : true,
+lazyLoadingInPrevNext : true,
         speed: 500,
        onInit: swiper => {
           // 初始化动画效果
@@ -16,7 +18,7 @@ $(function(){
           swiperAnimate(swiper); //初始化完成开始动画
 
           let bgColor = $(swiper.slides[swiper.activeIndex]).attr('data-background-color');
-          $body.css({"background-color":bgColor})
+          $body.css({"background-color":bgColor});
         }
   });
 
@@ -25,8 +27,19 @@ $(function(){
     swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
 
     // Change background-color
-    let bgColor = $(swiper.slides[swiper.activeIndex]).attr('data-background-color');
-    $body.css({"background-color":bgColor})
+    let $currentSlide = $(swiper.slides[swiper.activeIndex]);
+    let bgColor = $currentSlide.attr('data-background-color');
+    $body.css({"background-color":bgColor});
+
+    // let autoplayDelay = $currentSlide.attr('data-autoplay-delay');
+    //
+    // if(autoplayDelay != undefined)
+    // {
+    //   setTimeout(()=>{
+    //     swiper.slideNext();
+    //   }, autoplayDelay);
+    // }
+
   });
 
 })
